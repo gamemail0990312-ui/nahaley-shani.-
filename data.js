33625f0582968dad5f6acb25674fa115b2cb8176
gamemail0990312-ui <gamemail0990312@gmail.com>
2026-03-30 16:14:03 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,24 @@
 const items = [
     { 
         id: 1, 
-        name: "סנדלי בנים חומות", 
-        price: "₪160", 
-        category: "sandals", 
-        img: "images/brown_sandals.jpg", 
-        desc: "סנדלי עור איכותיים ונוחים במיוחד." 
+        name: "נעלי אלגנט גברים", 
+        price: "₪300", 
+        category: "elegant", 
+        img: "images/16.jpg", 
+        sizes: [24, 25, 26, 27, 28, 29, 30],
+        desc: "נעלי אלגנט לגברים מעור לשבתות וחגים." 
     },
     { 
         id: 2, 
-        name: "נעלי ספורט נייק", 
-        price: "₪350", 
+        name: "נעלי ספורט hoka", 
+        price: "₪החל מ-490", 
         category: "sport", 
-        img: "images/nike.jpg", 
+        img: "images/13.jpg", 
         desc: "נעלי ריצה קלות ומעוצבות." 
     },
     { 
         id: 3, 
-        name: "נעלי שבת לילדים", 
+        name: "נעלי שבת לילדות", 
         price: "₪120", 
         category: "kids", 
         img: "images/kids_shabbat.jpg", 
